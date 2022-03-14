12b31aa50a69527b24f5e8945a56fe89d21cdfcc
leeool <lgonsalez02@gmail.com>
2022-03-14 00:32:29 -0300

    finaliza o exericio

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿string frase;
+string palavracensurada = "$%@#@#&**$";
+
+Console.Write("Digite uma frase: ");
+frase = Console.ReadLine()!;
+
+frase = frase
+    .Replace("bobo", $"{palavracensurada}")
+    .Replace("boba", $"{palavracensurada}")
+    .Replace("feio", $"{palavracensurada}")
+    .Replace("feia", $"{palavracensurada}")
+    .Replace("inutil", $"{palavracensurada}")
+    .Replace("chato", $"{palavracensurada}")
+    .Replace("chata", $"{palavracensurada}")
+    .Replace("estranho", $"{palavracensurada}")
+    .Replace("estranha", $"{palavracensurada}")
+    .Replace("besta", $"{palavracensurada}")
+    .Replace("tonto", $"{palavracensurada}")
+    .Replace("tonta", $"{palavracensurada}");
+
+Console.Write($"\n{frase}");
